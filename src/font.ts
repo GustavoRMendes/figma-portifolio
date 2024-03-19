196ed583f4b099eb09ee440963dffb95ac6fd0b1
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-18 20:55:31 -0400

    created header

---
+++ src/font.ts
@@ -0,0 +1,10 @@
+import { Fira_Sans } from 'next/font/google'
+
+
+export const fontFiraSans = Fira_Sans({
+  weight: ['400','700'],
+  variable: '--font-fira',
+  subsets: ['latin'],
+  display:'swap',
+  style:'normal',
+})
